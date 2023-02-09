2b7576a309aa854162b029c9cf096c80c17863a9
Arthur Correia <arthurcorria0109@gmail.com>
2023-02-09 14:54:39 -0300

    change

--- modulo-02/10-type-boolean/booleanType.ts
+++ modulo-02/10-type-boolean/booleanType.ts
@@ -1,16 +1,13 @@
-// // exemplo 1
-// let tarefaCompleta: boolean = true;
-// let tarefaIncompleta: boolean = false;
+// exemplo 1
+let tarefaCompleta: boolean = true;
+let tarefaIncompleta: boolean = false;
 
-// console.log(tarefaCompleta);
-// console.log(tarefaIncompleta);
+console.log(tarefaCompleta);
+console.log(tarefaIncompleta);
 
-// // exemplo 2
-// const exibirResultado = (maiorDeIdade: boolean) => maiorDeIdade ? console.log('Apto a dirigir.') : console.log('Inapto a dirigir');
+// exemplo 2
+const exibirResultado = (maiorDeIdade: boolean) => maiorDeIdade ? console.log('Apto a dirigir.') : console.log('Inapto a dirigir');
 
-// const verificarIdade = (age: number) => age >= 18 ? true : false;
+const verificarIdade = (age: number) => age >= 18 ? true : false;
 
-// exibirResultado(verificarIdade(30));
-
-const test: number = Infinity;
-console.log(test);
+exibirResultado(verificarIdade(30));
